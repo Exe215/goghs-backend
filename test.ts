89940da2e6dae64f754dd8e2bf7cc9c009f7c616
jozark <joshua.meyer.personal@gmail.com>
2023-02-25 15:52:30 +0100

    add husky and mocha

---
+++ test.ts
@@ -0,0 +1,22 @@
+import assert from "assert";
+import { indexPathEqual } from "./helper";
+
+describe("indexPathEqual", function () {
+  it("should return true for identical index paths", function () {
+    const a = [1, 3];
+    const b = [1, 2, 3];
+    assert.equal(indexPathEqual(a, b), true);
+  });
+
+  it("should return false for different index paths", function () {
+    const a = [1, 2, 3];
+    const b = [1, 2, 4];
+    assert.equal(indexPathEqual(a, b), false);
+  });
+
+  it("should return false for index paths of different lengths", function () {
+    const a = [1, 2, 3];
+    const b = [1, 2];
+    assert.equal(indexPathEqual(a, b), false);
+  });
+});
